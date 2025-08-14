08282a24c9f346763a825b1b31fa86e317b570dc
tony <tony1@kinwong.com>
2025-08-14 17:32:07 +0800

    feat:DataOps整理

--- 数据BI/常用组件/非开源/Fine BI/scripts/finebi_datasync_trigger.py
+++ 数据BI/常用组件/非开源/Fine BI/scripts/finebi_datasync_trigger.py
@@ -1,50 +1,29 @@
-""" 
-================================================
-# author: chen liu
-# purpose : 同步抽取BI数据
-# create_at : 2024-12-25
-========================================================
-
-========================================================
-
+# -*- coding: utf-8 -*-
 
 """
-
+descr: Fine BI 数据同步触发脚本
+author: Tony
+date: 2025-08-12
+"""
 
 import requests
 import json
-import sys
 
-# Absolute path to your kw_airflow_utils.py
-kw_utils_path = "/root/airflow/dags" # Adjust if needed
+host = '10.53.1.173'
+port = '8080'
+user = '000000'
+password = 'PddHSuNXLHRl7etM2DZ5kg=='
+baseUrl = 'http://' + host + ':' + port
+print("baseUrl:{}".format(baseUrl))
 
-# Add the directory to the PYTHONPATH
-sys.path.append(kw_utils_path)
-
-from kw_airflow_utils import KwUtils # or from kw_airflow_utils import KwUtils if it's a class
-
-
-#1. 读取配置文件
-ini_path =  kw_utils_path + "/profile_kw.ini"
-impala_ini = KwUtils.config_read_ini("BI", ini_path)
-host = impala_ini[0]
-port = impala_ini[1]
-user = impala_ini[2]
-password = impala_ini[3]
-origin = 'http://'+host+':'+port
-
-# 读取 py脚本外接参数 ,第一个参数是表名；第二个参数是抽取类型，没有则默认为1
-table_name = sys.argv[1]
-updateType = sys.argv[2] if len(sys.argv)>1 else '1'
-
-#2. 登录获取accessToken
+# ###################模拟登陆
+#. 登录获取accessToken
 session = requests.Session()
-
 headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'Connection': 'keep-alive',
-    'Origin': origin,
-    'Referer': origin+'/decision/login',
+    'Origin': baseUrl,
+    'Referer': baseUrl + '/decision/login',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
@@ -65,11 +44,13 @@ json_data = {
     'encrypted': True,
 }
 
-response = session.post(origin+'/decision/login', headers=headers, json=json_data)
+# 登录请求接口
+loginUrl = baseUrl + '/decision/login'
+print("loginUrl:{}".format(loginUrl))
 
-#json 解析 response.content，获取accessToken
+response = session.post(loginUrl, headers=headers, json=json_data)
+# json 解析 response.content，获取accessToken
 data = json.loads(response.content)
-print(data)
 if response.status_code == 200 :
     print('get accessToken success')
 else:
@@ -77,8 +58,9 @@ else:
     raise Exception('get accessToken failed')
 accessToken = data['data']['accessToken']
 
-
-#3. 开始同步抽取数据
+# ##################################################### 模拟成功登录之后的操作
+table_name = '8fecd8cae2454e5d8cef41f3ea14cf40'
+updateType = '2'
 
 cookies = {
     'tenantId': 'default',
@@ -86,7 +68,6 @@ cookies = {
     'dev': 'UqNTp725yS2lMBaBDPL3WY7tLd',
     'fine_login_users': 'f-9200302300158213064,4340becf-ca56-4f60-b9f9-653c8a47a02a',
     'fine_auth_token': accessToken,
-
 }
 
 json_data = {
@@ -97,8 +78,8 @@ json_data = {
 headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
     'Connection': 'keep-alive',
-    'Origin': origin,
-    'Referer': origin+'/decision',
+    'Origin': baseUrl,
+    'Referer': baseUrl + '/decision',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
@@ -113,11 +94,13 @@ headers = {
     'x-requested-with': 'XMLHttpRequest',
 }
 
-response = session.post(origin+'/decision/v5/conf/update/tables/'+table_name+'/trigger', cookies=cookies, headers=headers, json=json_data)
-print(response.content)
+# 数据同步更新接口
+dataSyncTriggerUrl = baseUrl + '/decision/v5/conf/update/tables/'+table_name+'/trigger'
+print("dataSyncTriggerUrl:{}".format(dataSyncTriggerUrl))
+response = session.post(dataSyncTriggerUrl, cookies=cookies, headers=headers, json=json_data)
 
 #解析response.content 提取 code
-#try except 如果返回的不是200 则失败
+# try except 如果返回的不是200 则失败
 data = json.loads(response.content)
 if data['code'] == '200':
     print('success')
